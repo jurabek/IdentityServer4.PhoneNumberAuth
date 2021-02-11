641b10606af4e82f60de3512d26e98b5ecca4c1d
Sérgio Azevedo <sergio.azevedo@herzsache.com>
2021-02-11 17:43:17 +0000

    Update dependencies

--- IdentityServer4.PhoneNumberAuth/Startup.cs
+++ IdentityServer4.PhoneNumberAuth/Startup.cs
@@ -6,6 +6,7 @@ using IdentityServer4.PhoneNumberAuth.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
